1d14779708e22bfba109cc69233e8eedbb9e4fc9
Delphia Aryana <112560412+DelphiaAryana@users.noreply.github.com>
2023-04-08 06:15:00 +0800

    Add files via upload

--- TugasLaravel_Delphia/routes/web.php
+++ TugasLaravel_Delphia/routes/web.php
@@ -1,1 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DataSkincare;
+use App\Http\Controllers\SkincareController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/login', [LoginController::class, 'show'])->name('login');
+Route::post('/login', [LoginController::class, 'auth'])->name('login.auth');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::group(['middleware' => ['admin']], function(){
+        Route::get('/data-pengguna', [DashboardController::class, 'showDataPengguna'])->name('dashboard.showDataPengguna');
+        Route::resource('dashboard', DashboardController::class)->name('*', 'dashboard');  
+        Route::get('/data-skincare', [DashboardController::class, 'showDataSkincare'])->name('dashboard.showDataSkincare');
+        Route::resource('data_skincare', DataSkincare::class)->name('*', 'data_skincare');
+    });
+
+    Route::get('/tips', function(){
+        return view('tips');
+    })->name('tips');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/skincare-recomendation', [SkincareController::class, 'showDataSkincare'])->name('skincare.showDataSkincare');
+});
 
