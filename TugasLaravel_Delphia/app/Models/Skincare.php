d9cf256a8dd71fc89bfe4bbc565a07cb1dd2fa0a
Delphia Aryana <112560412+DelphiaAryana@users.noreply.github.com>
2023-04-08 05:47:49 +0800

    Add files via upload

--- TugasLaravel_Delphia/app/Models/Skincare.php
+++ TugasLaravel_Delphia/app/Models/Skincare.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Skincare extends Model
+{
+    use HasFactory;
+    protected $table = 'skincare';
+
+    protected $fillable = ['nama', 'fungsi', 'harga', 'image'];
+}
